aeabf4599a502b2709a328da8de4312cc5946cc0
gangireddydanam <gangireddydanam@gmail.com>
2024-03-28 08:46:00 +0530

    key pair added

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@ resource "aws_instance" "this" {
   ami = "ami-0440d3b780d96b29d"
   instance_type = "t2.micro"
   user_data = file("tomcat_install.sh")
+  key_name = "eks-helm"
   tags = {
     Name = "tomcat"
+    terraform = "true"
   }
 }
