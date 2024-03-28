99369877433af3f7c8365399da96384a3e66b0e3
gangireddydanam <gangireddydanam@gmail.com>
2024-03-28 09:04:51 +0530

    updated script

--- tomcat_install.sh
+++ tomcat_install.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
-#pre-req java 
 #!/bin/bash 
 #install java 
 sudo yum install java -y 
 #we will install with zip/binary
 cd /opt
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.84/bin/apache-tomcat-9.0.84-windows-x64.zip
-unzip apache-tomcat-9.0.84-windows-x64.zip
-rm -f apache-tomcat-9.0.84-windows-x64.zip
-mv apache-tomcat-9.0.84 tomcat9
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.87/bin/apache-tomcat-9.0.87-windows-x64.zip
+unzip apache-tomcat-9.0.87-windows-x64.zip
+rm -f apache-tomcat-9.0.87-windows-x64.zip
+mv apache-tomcat-9.0.87 tomcat9
+chown -R ec2-user:ec2-user tomcat9
 cd tomcat9/
 cd bin
 chmod 755 *.sh
-sh startup.sh
+su - ec2-user "sh startup.sh"
 ps -ef | grep -i tomcat
 sudo netstat -anlp | grep ":8080"
